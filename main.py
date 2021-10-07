963c4567685daea78c3fb3e6b970a0e5b430ea26
徐焱挺 <xuyantiong@lidig.com>
2021-10-07 15:13:42 +0800

    多指数回测

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import datetime
 import math
+# import multiprocessing
+from os import walk
 
 import backtrader as bt
 import pandas as pd
@@ -71,11 +73,11 @@ class Strategy_percent(bt.Strategy):
         if self.order:
             return  # pending order execution
 
-        close_last_100 = self.dataclose.get(size=120)
-        volume_last_100 = self.datavolume.get(size=120)
-        high_last_100 = self.datahigh.get(size=120)
-        low_last_100 = self.datalow.get(size=120)
-        if len(close_last_100) and len(volume_last_100) > 0:
+        close_last_100 = self.dataclose.get(size=100)
+        volume_last_100 = self.datavolume.get(size=100)
+        high_last_100 = self.datahigh.get(size=100)
+        low_last_100 = self.datalow.get(size=100)
+        if len(close_last_100) and len(volume_last_100) > 60:
             fund_index_hist_sina_df = pd.DataFrame(
                 {"close": close_last_100, 'amount': volume_last_100, "high": high_last_100, "low": low_last_100})
             strategy_new_result = yt_strategy.strategy_combine(fund_index_hist_sina_df)
@@ -86,9 +88,9 @@ class Strategy_percent(bt.Strategy):
         data_value = self.broker.get_value([self.data])
         dt = self.data.datetime.date()
         portfolio_value = self.broker.get_value()
-        print('%04d - %s - Position Size:     %02d - Value %.2f' %
-              (len(self), dt.isoformat(), self.position.size, portfolio_value))
-        print('data_value', data_value)
+        # print('%04d - %s - Position Size:     %02d - Value %.2f' %
+        #       (len(self), dt.isoformat(), self.position.size, portfolio_value))
+        # print('data_value', data_value)
         # self.log("最大回撤:-%.2f%%" % self.stats.drawdown.maxdrawdown[-1], doprint=True)
 
 
@@ -124,7 +126,7 @@ def runstart():
     dt_end = datetime.datetime.strptime("20210927", "%Y%m%d")
     # Pass it to the backtrader datafeed and add it to the cerebro
     data = bt.feeds.GenericCSVData(
-        dataname=r'./399006.csv',
+        dataname=r'./index_history_data/930608.csv',
         fromdate=dt_start,  # 起止日期
         todate=dt_end,
         nullvalue=0.0,
@@ -158,6 +160,8 @@ def runstart():
     results = cerebro.run()
     thestrat = results[0]
     #
+    nhsy = thestrat.analyzers.AR.get_analysis()
+    print(type(nhsy))
     print("年化收益率:", thestrat.analyzers.AR.get_analysis())
     print("夏普:", thestrat.analyzers.SR.get_analysis()['sharperatio'])
     print("最大回撤:%.2f，最大回撤周期%d" % (
@@ -170,13 +174,82 @@ def runstart():
     pnl_won = trade_info['won']['pnl']['total']
     pnl_lost = trade_info['lost']['pnl']['total']
     print('交易次数:', total_trade_num)
-    print('胜率:', win_num / total_trade_num, '败率:', lost_num / total_trade_num, '盈亏比:',
-          (pnl_won / win_num) / ((- pnl_lost) / lost_num))
+    print('胜率:', win_num / total_trade_num, '败率:', lost_num / total_trade_num)
+    print('盈亏比:', (pnl_won / win_num) / ((- pnl_lost) / lost_num))
     # print('SQN:', thestrat.analyzers.SQN.get_analysis().sqn)
     # print('败率:', lost_num / total_trade_num)
     # print('盈亏比:', pnl_won / - pnl_lost)
     #
 
 
+def loop_index_history(index):
+    # Create a cerebro entity
+    cerebro = bt.Cerebro()
+
+    # Add a strategy
+    # cerebro.addstrategy(TestStrategy)
+    cerebro.addstrategy(Strategy_percent)
+    # cerebro.addstrategy(Benchmark)
+    # Get a pandas dataframe
+    dt_start = datetime.datetime.strptime("20180101", "%Y%m%d")
+    dt_end = datetime.datetime.strptime("20210927", "%Y%m%d")
+    # Pass it to the backtrader datafeed and add it to the cerebro
+    data = bt.feeds.GenericCSVData(
+        dataname=r'./index_history_data/' + index,
+        fromdate=dt_start,  # 起止日期
+        todate=dt_end,
+        nullvalue=0.0,
+        dtformat=('%Y-%m-%d'),  # 日期列的格式
+        datetime=0,  # 各列的位置，从0开始，如列缺失则为None，-1表示自动根据列名判断
+        high=3,
+        low=4,
+        open=1,
+        close=2,
+        volume=6,
+        openinterest=-1
+    )
+
+    cerebro.adddata(data)
+    cerebro.broker.setcash(100000.0)
+    # 手续费
+    cerebro.broker.setcommission(commission=0.0003)
+    # 滑点
+    cerebro.broker.set_slippage_fixed(0.01)
+    cerebro.addobserver(bt.observers.DrawDown)
+    cerebro.addanalyzer(btanalyzers.AnnualReturn, _name="AR")
+    results = cerebro.run()
+    thestrat = results[0]
+    #
+    nhsy = thestrat.analyzers.AR.get_analysis()
+    return nhsy
+
+
+# def handle_multi(files):
+#     pool = multiprocessing.pool.ThreadPool(len(files))
+#     try:
+#         res = pool.map(loop_index_history, files)
+#     finally:
+#         pool.close()
+#     return [d for d in res if d is not None]
+
+
 if __name__ == '__main__':
-    runstart()
+    dir_path = r'./index_history_data/'
+    filenames = next(walk(dir_path), (None, None, []))[2]
+    # res = handle_multi(filenames)
+    # print(res)
+    yearly = None
+    for index, file in enumerate(filenames):
+        print(file)
+        index_yearly = loop_index_history(file)
+        index_yearly_pd = pd.DataFrame(dict(index_yearly), index=[0])
+        if yearly is None:
+            yearly = index_yearly_pd
+        else:
+            yearly.append(index_yearly_pd, ignore_index=True)
+        print(str(index + 1) + '/' + str(len(filenames)))
+    for year in yearly.columns:
+        year_mean = yearly[year].mean()
+        print(year, year_mean)
+# if __name__ == '__main__':
+#     runstart()
